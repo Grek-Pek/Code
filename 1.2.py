9dc61291ff77f826b7bb4be88f91dbfec0fd1dd6
Grek-Pek <149669178+Grek-Pek@users.noreply.github.com>
2023-11-02 19:17:49 +0900

    1.2.py

---
+++ 1.2.py
@@ -0,0 +1,17 @@
+salary = float(input('Зарплата за месяц: '))
+weekend_hours = int(input('Количество отработанных в выходные часов: '))
+bonus_rate = 0.10
+bonus_amount = salary * 0.01 * weekend_hours
+print('Размер премии', bonus_amount)
+
+
+
+
+
+
+
+
+
+
+
+
