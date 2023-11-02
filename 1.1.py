7bd66e609bba1c515ad7256a36e7bf159e366a31
Grek-Pek <149669178+Grek-Pek@users.noreply.github.com>
2023-11-02 19:04:10 +0900

    1.2

--- 1.1.py
+++ 1.1.py
@@ -1,9 +1,3 @@
-movie_name = input("Enter the movie name:")
-theater_name = input("Enter the theater name:")
-show_time = input("Enter the time of the show:")
-message = " Билет на " + movie_name + " в " + theater_name + "  на  " + show_time + " забронирован "
-
-print(message)
 
 
 
