bcd9db1da72d03033d835b008d4a38dbb632344e
Grek-Pek <149669178+Grek-Pek@users.noreply.github.com>
2023-11-29 18:08:04 +0900

    2.2

---
+++ 2.2.py
@@ -0,0 +1,8 @@
+string1 = input("Введите первую строку: ")
+string2 = input("Введите вторую строку: ")
+string3 = input("Введите третью строку: ")
+if (string1 == "раз" or string1 == "1") and (string2 == "два" or string2 == "2") and (
+        string3 == "три" or string3 == "3"):
+    print("ГОРИ")
+else:
+    print("НЕ ГОРИ")
