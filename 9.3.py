10db5aba1e0e985ec94211b15703418cc5a45214
Grek-Pek <149669178+Grek-Pek@users.noreply.github.com>
2023-12-07 19:19:33 +0900

    9.3

---
+++ 9.3.py
@@ -0,0 +1,11 @@
+while True:
+    try:
+        num1 = int(input("Введите первое число: "))
+        num2 = int(input("Введите второе число: "))
+        result = num1 + num2 , num1 // num2
+        print("Результат сложения:", result)
+        break
+    except ValueError:
+        print("Ошибка: введены некорректные данные. Пожалуйста, введите только целочисленные значения.")
+    except ZeroDivisionError:
+        print("Ошибка: деление на ноль невозможно.")
